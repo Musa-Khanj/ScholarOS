7de5245d7437e6927318141d48db3d0362deefbc
Musa-Khanj <musakh828@gmail.com>
2026-07-23 11:16:38 +0500

    feat(events): implement event bus and publish/subscribe system

---
+++ scholaros/events/__init__.py
@@ -0,0 +1,9 @@
+from scholaros.events.bus import EventBus
+from scholaros.events.event import Event
+from scholaros.events.service import EventService
+
+__all__ = [
+    "Event",
+    "EventBus",
+    "EventService",
+]
